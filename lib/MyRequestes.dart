bb0a873b8219b43d3019f3851d592f5a94618d24
wiam bou <wiam.bou67@gmail.com>
2023-05-09 23:58:00 +0100

    design

--- lib/MyRequestes.dart
+++ lib/MyRequestes.dart
@@ -22,6 +22,7 @@ class _MyRequestesState extends State<MyRequestes> {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Requests & Responses"),
+        backgroundColor: Colors.deepPurple,
       ),
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
@@ -37,6 +38,13 @@ class _MyRequestesState extends State<MyRequestes> {
             label: 'My Responses',
           )
         ],
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: TextStyle(fontSize: 16.0),
+        unselectedLabelStyle: TextStyle(fontSize: 14.0),
+        backgroundColor: Colors.white,
+        elevation: 10,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
@@ -67,6 +75,7 @@ class MyRequestsPage extends StatelessWidget {
         final exchangeDocs = snapshot.data!.docs;
 
         return ListView.builder(
+
           itemCount: exchangeDocs.length,
           itemBuilder: (context, index) {
             final exchangeDoc = exchangeDocs[index];
@@ -78,13 +87,58 @@ class MyRequestsPage extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
+              color: Colors.grey[300], // ajout de la couleur de fond grise
               child: ListTile(
-                leading: Icon(Icons.person),
-                title: Text((exchangeData
-                    as Map<String, dynamic>)['proposerProductName']),
-                subtitle:
-                    Text((exchangeData as Map<String, dynamic>)['description']),
-                trailing: Text("re"),
+                contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0), // ajout du padding du contenu
+                leading: FutureBuilder<DocumentSnapshot>(
+                  future: FirebaseFirestore.instance.collection('products').doc((exchangeData as Map<String, dynamic>)['proposerProductId']).get(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                      var productData = snapshot.data!.data() as Map<String, dynamic>;
+                      var productImage = productData['image'];
+
+                      return CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(productImage),
+                      );
+                    } else {
+                      return CircleAvatar(
+                        radius: 50,
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+
+                title: Text(
+                  (exchangeData as Map<String, dynamic>)['proposerProductName'],
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold, // ajout du style du texte en gras
+                  ),
+                ),
+                subtitle: Text(
+                  (exchangeData as Map<String, dynamic>)['description'],
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+
+                trailing: IconButton(
+                  icon: Icon(Icons.arrow_forward_ios),
+                  color: Colors.deepPurple,
+                  onPressed: () {
+                    // Ajoutez ici le code à exécuter lorsqu'on appuie sur l'icône "info"
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ConfermationPage(exchangeId: exchangeDoc.id)),
+                    );
+                  },
+                ),
                 onTap: () {
                   Navigator.push(
                     context,
@@ -96,8 +150,15 @@ class MyRequestsPage extends StatelessWidget {
                 selected: true, // Whether the ListTile is selected or not
                 enabled: true, // Whether the ListTile is enabled or not
                 dense: false, // Whether the ListTile has reduced height or not
+                selectedTileColor: Colors.deepPurple[50], // ajout de la couleur de sélection pour le ListTile
+                shape: RoundedRectangleBorder( // ajout d'une bordure pour le ListTile
+
+                ),
+                tileColor: Colors.white, // ajout de la couleur d'arrière-plan pour le ListTile
+                horizontalTitleGap: 10.0, // ajout de l'espace horizontal entre le leading et le title
               ),
             );
+
           },
         );
       },
